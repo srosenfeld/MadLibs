db9d2824f15559c1f438d30f019a7019d3887347
srosenfeld <srosenfeld@users.noreply.github.com>
2017-10-24 20:57:05 -0400

    New MadLib file
    example file

---
+++ madlib_SR.py
@@ -0,0 +1,28 @@
+### Mad Libs ###
+### Source: https://i.pinimg.com/736x/34/38/85/3438857754a71a759ad673a2b9c572b0--holiday-games-christmas-games.jpg
+
+print("Write an adjective")
+adj1 = input()
+
+print("Write an adjective")
+adj2 = input()
+
+print("Name a room")
+room1 = input()
+
+print("Write a plural noun.")
+pluralnoun1 = input()
+
+print("Write another plural noun.")
+pluralnoun2 = input()
+
+print("Write a noun.")
+noun1 = input()
+
+
+### BEGIN MAD LIB ###
+
+print("No Christmas season can really " + adj1 + " unless you have a/an " + adj2 + " tree in your " + room1 + ".")
+print("If you live in a city, you will see many vacant " + pluralnoun1 + " filled with hundreds of " + pluralnoun2 + " for sale.")
+print("If you live in the country, you can get your own " + noun1 + " in forests.")
+
